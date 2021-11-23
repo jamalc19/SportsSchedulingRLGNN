4f749a925d5646bb0dd727e5756143e06a89388e
jamalc19 <jamal.chu@mail.utoronto.ca>
2021-11-23 12:39:03 -0500

    s2v parameters retain gradients and actually train. Modification to graph to just use hard constraints for testing purposees. Batch training incomplete

--- training.py
+++ training.py
@@ -21,29 +21,23 @@ from s2v_scheduling import Model
 
 #Training params
 GAMMA = 0.999
-EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 200
+EPS_START = 0.05
+EPS_END = 0.001
+EPS_DECAY = 1000
 TRAINING_DELAY = 1
-EPISODES = 100
+EPISODES = 1000000
 BATCH_SIZE = 128
 TARGET_UPDATE = 3
+N_STEP_LOOKAHEAD=10
 
 #Agent params
 EMBEDDING_SIZE = 64
-CACHE_SIZE = 1000
-
-
-
-#TODO Decaying epsilon, relay memory, remove constants from global
-#EPS_START = 0.9
-#EPS_END = 0.05
-#EPS_DECAY = 200
+CACHE_SIZE = 100
 
 
 #Declare training instances
 #instances= os.listdir('PreprocessedInstances/')
-instances= ['ITC2021_Test1.pkl'] #testing on just one instance for now
+instances= ['OnlyHardITC2021_Test1.pkl'] #testing on just one instance for now
 
 #Use cuda
 #FIXME device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -77,9 +71,9 @@ class Agent:
         if self.use_cuda:
             self.net = self.model.to(device="cuda")   
 
-        self.optimizer = optim.SGD(self.model.parameters(), lr=0.01, momentum=0.9)#TODO tune params
-        #self.loss_fn = nn.SmoothL1Loss()
-        self.loss_fn = nn.MSELoss()
+        self.optimizer =optim.RMSprop(self.model.parameters())#TODO tune params
+        self.loss_fn = nn.SmoothL1Loss()
+        #self.loss_fn = nn.MSELoss()
 
 
     def Q(self,graph,model='model'):
@@ -134,31 +128,17 @@ class Agent:
         """
         return max(q_value_dict, key=q_value_dict.get)
 
-    def cache(self, state, next_state, action, reward, done):
+    def cache(self, instance,partialsolution,action):
         """
         Store the experience to self.memory (replay buffer)
+        Transitions are deterministic, so do not need to store rewards or next state.
 
         Inputs:
-        state (list),
-        next_state (list),
-        action (list),
-        reward ()
-        done(bool)
+        instance (str),
+        partialsolution (set),
+        action (int)
         """
-        if self.use_cuda:
-            state = torch.tensor(state).cuda()
-            next_state = next_state.clone().detach().cuda()
-            action = action.clone().detach().cuda()
-            reward = torch.tensor([reward]).cuda()
-            done = torch.tensor([done]).cuda()
-        else:
-            state = torch.tensor(state)
-            next_state = next_state.clone().detach()
-            action = action.clone().detach()
-            reward = torch.tensor([reward])
-            done = torch.tensor([done])
-
-        self.memory.append((state, next_state, action, reward, done,))
+        self.memory.append((instance,partialsolution,action))
 
 
     def recall(self):
@@ -174,9 +154,9 @@ class Agent:
             done:
         """
         batch = random.sample(self.memory, BATCH_SIZE)
-        state, next_state, action, reward, done = map(torch.stack, zip(*batch))
+        #state, next_state, action, reward, done = map(torch.stack, zip(*batch))
 
-        return state, next_state, action.squeeze(), reward.squeeze(), done.squeeze()
+        return batch
 
     def learn(self, y, Q):
         """Backwards pass for model"""
@@ -184,56 +164,104 @@ class Agent:
         # state, next_state, action, reward, done = self.recall()
                 
         loss = self.loss_fn(Q, y)
-        print(loss)
         self.optimizer.zero_grad()
         loss.backward()
+        #print(loss.item())
         self.optimizer.step()
         return
         # return loss.item()
+
+    def batch_train(self):
+        batch = self.recall()
+        self.optimizer.zero_grad()
+        for instance, partial_solution, action in batch:
+            # get to current state
+            graph = pickle.load(open('PreprocessedInstances/' + i, 'rb'))
+            for node_id in partial_solution:
+                graph.selectnode(node_id)
+            #forward pass through the network
+            q_value_dict, graph_embeddings = agent.Q(graph)
+            #get next state and reward
+            reward, done = graph.selectnode(action)
+
+            # compute Q value of next state
+            if done:
+                nextstateQ = torch.zeros(1)
+            elif len(graph.nodedict) + len(graph.solution) < graph.solutionsize:  # RL agent reached an infeasible solution
+                nextstateQ = torch.tensor(-1.0)
+            else:
+                nextstateQ = max(agent.Q(graph, model='target')[0].values())  # get next state Qvalue with target network
+            agent.learn(nextstateQ + reward, q_value_dict[node_to_add])
+
+            loss = self.loss_fn(q_value_dict[action], nextstateQ + reward)/BATCH_SIZE
+
+            loss.backward()
+        self.optimizer.step()
+
+    def rollout(self, i):
+        with torch.no_grad():
+            graph = pickle.load(open('PreprocessedInstances/' + i, 'rb'))
+
+            done = False
+            cumulative_reward = -graph.costconstant
+            while not done:
+                q_value_dict, graph_embeddings = agent.Q(graph)
+                node_to_add = agent.greedy(q_value_dict)
+                action = graph_embeddings[node_to_add]
+                # Take action, recieve reward
+                reward, done = graph.selectnode(node_to_add)
+                cumulative_reward += reward
+                if len(graph.nodedict) < graph.solutionsize:  # RL agent reached an infeasible solution
+                    print('infeasible')
+                    done = True
+        return cumulative_reward
+
 
 
 #***********************************************************************
 #TRAINING
 #***********************************************************************
+warmstart=False
+#warmstart = 'ModelParams/firsttestwarmstart1000'
 if __name__=='__main__':
 
     #Create agent
     agent = Agent()
+    if warmstart:
+        agent.model.load_state_dict(torch.load(warmstart))
 
     np.random.seed(0)
     torch.manual_seed(0)
     #Training loop
+
     for e in range(EPISODES):
         i = instances[np.random.randint(0,len(instances))] #sample the next instance from a uniform distribution
         graph = pickle.load(open('PreprocessedInstances/'+i,'rb'))
 
-        state = np.zeros(EMBEDDING_SIZE)
         done = False
         t = 1 #TODO set training delay
-        cumulative_reward = -graph.costconstant #TODO verify that this is the correct implementation. It was but I defined some constraints by giving the graph an initial cost
-
+        cumulative_reward = -graph.costconstant
         #Training
         while not done:
-
             #Determine which action to take
             q_value_dict, graph_embeddings = agent.Q(graph)
-            node_to_add= agent.greedyepsilon(q_value_dict,t) #node_to_add is the selected nodeid, action is the nodes s2v embedding
-            action = graph_embeddings[node_to_add]
+            #node_to_add= agent.greedy(q_value_dict) #node_to_add is the selected nodeid, action is the nodes s2v embedding
+            node_to_add = agent.greedyepsilon(q_value_dict,t)  # node_to_add is the selected nodeid, action is the nodes s2v embedding
 
+            #Cache state and action
+            agent.cache(i, graph.solution, node_to_add)
 
             #Take action, recieve reward
             reward, done = graph.selectnode(node_to_add)
             cumulative_reward+=reward
 
-            #Cache result
-            #agent.cache(state, next_state, action, reward, done)
-
             #Train
             if t >= TRAINING_DELAY:
+                #agent.batch_train()
                 if done:
                     nextstateQ=torch.zeros(1)
                 elif len(graph.nodedict) + len(graph.solution) < graph.solutionsize: #RL agent reached an infeasible solution
-                    nextstateQ = torch.tensor(-100000.0)
+                    nextstateQ = torch.tensor(-1.0)
                 else:
                     nextstateQ = max(agent.Q(graph,model='target')[0].values()) #get next state Qvalue with target network
                 agent.learn(nextstateQ+reward,q_value_dict[node_to_add])
@@ -250,5 +278,10 @@ if __name__=='__main__':
             #Update state
             #state = next_state
         print(e,cumulative_reward)
+
         if e% 100 ==0:
-            torch.save(agent.model.state_dict(), 'ModelParams/firsttest{}'.format(e))
+            torch.save(agent.model.state_dict(), 'ModelParams/s2visactuallytraining{}'.format(e))
+
+        if e%10==0:#rollout
+            cumulative_reward=agent.rollout(i)
+            print('Rollout Cumulative Reward:',cumulative_reward)
